652301fbe1d1552095aff120c4093086e1973bc3
Nick Fitzpatrick <nickf5624@gmail.com>
2024-09-11 23:16:16 -0700

    intro mod

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -20,7 +20,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Hello. This is my first Next.js App, on Vercel</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
